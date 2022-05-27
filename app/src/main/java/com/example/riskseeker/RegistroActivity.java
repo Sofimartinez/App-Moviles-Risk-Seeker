54cd9fb826460865418f0b76ebff65085657756c
awwa121 <roberto.covarrubias2017@gmail.com>
2022-05-27 00:37:18 -0400

    mensajes de advertencia

--- app/src/main/java/com/example/riskseeker/RegistroActivity.java
+++ app/src/main/java/com/example/riskseeker/RegistroActivity.java
@@ -42,35 +42,43 @@ public class RegistroActivity extends AppCompatActivity {
 
     public void registrarUsuario(View view){
 
-        if(contrasena.getText().toString().equals(contrasenaConfirmacion.getText().toString())){
-
-            mAuth.createUserWithEmailAndPassword(correo.getText().toString().trim(), contrasena.getText().toString())
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                // Sign in success, update UI with the signed-in user's information
-                                FirebaseUser user = mAuth.getCurrentUser();
-                                Toast.makeText(getApplicationContext(), "Usuario Registrado",
-                                        Toast.LENGTH_SHORT).show();
-                                Intent i = new Intent(getApplicationContext(),MainActivity.class);
-                                startActivity(i);
-                                //updateUI(user);
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Toast.makeText(getApplicationContext(), "Authentication failed.",
-                                        Toast.LENGTH_SHORT).show();
-                                //updateUI(null);
-                            }
-
-                            // ...
-                        }
-                    });
-
-
+        String contra = contrasena.getText().toString();
+        if(contra.length()<6) {
+            contrasena.setError(getString(R.string.contraminimo));
+            Toast.makeText(this,"La contraseña debe tener mínimo 6 caracteres",Toast.LENGTH_LONG).show();
         }else{
-            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+            if(contrasena.getText().toString().equals(contrasenaConfirmacion.getText().toString())){
+
+                mAuth.createUserWithEmailAndPassword(correo.getText().toString().trim(), contrasena.getText().toString())
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Toast.makeText(getApplicationContext(), "Usuario Registrado",
+                                            Toast.LENGTH_SHORT).show();
+                                    Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                                    startActivity(i);
+                                    //updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(getApplicationContext(), "Registro fallido",
+                                            Toast.LENGTH_SHORT).show();
+                                    //updateUI(null);
+                                }
+
+                                // ...
+                            }
+                        });
+
+
+            }else{
+                Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+            }
+
         }
 
     }
+
 }
