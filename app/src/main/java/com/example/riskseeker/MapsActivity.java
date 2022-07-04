21ba3b795b2bd4a749478c54de70a522df927f95
fvalladaresj <fvalladares.j@gmail.com>
2022-07-04 18:01:16 -0400

    cambios heatmap

--- app/src/main/java/com/example/riskseeker/MapsActivity.java
+++ app/src/main/java/com/example/riskseeker/MapsActivity.java
@@ -1,5 +1,7 @@
 package com.example.riskseeker;
 
+import static com.example.riskseeker.BuildConfig.MAPS_API_KEY;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
@@ -7,6 +9,7 @@ import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Criteria;
 import android.location.Geocoder;
@@ -34,6 +37,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -45,11 +49,25 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.maps.android.SphericalUtil;
+import com.google.maps.android.heatmaps.Gradient;
 import com.google.maps.android.heatmaps.HeatmapTileProvider;
+import com.google.maps.android.heatmaps.WeightedLatLng;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener, GoogleMap.OnMarkerClickListener {
 
@@ -114,7 +132,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
             actualizarUbicacion();
-            }
+        }
         else{
             activarUbicacion();
         }
@@ -256,8 +274,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     //Heatmap
     private void heatMap(GoogleMap googleMap) {
         map = googleMap;
-        
+
         List<LatLng> reports = new ArrayList<>();
+        List<String> pathValuesInterpolated = new ArrayList<>();
 
         String TAG = "readData";
         // Referencia a reportes
@@ -269,7 +288,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // Obtiene datos y se actualiza 
+                // Obtiene datos y se actualiza
                 for(DataSnapshot ds : dataSnapshot.getChildren()) {
 
                     String type = ds.child("tipo").getValue(String.class);
@@ -277,80 +296,139 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     Double lon = ds.child("longitud").getValue(Double.class);
                     String idReporte = ds.child("idReporte").getValue(String.class);
 
-                    reports.add(new LatLng(lat, lon));
+                        reports.add(new LatLng(lat, lon));
 
-                    //Añadir Marcadores
-                    switch (type) {
-                        case "Hurto":
-                            map.addMarker(new MarkerOptions()
-                                    .position(new LatLng(lat, lon))
-                                    .title(type)
-                                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)))
-                                    .setTag(idReporte);
-                            break;
-                        case "Actividad sospechosa":
-                            map.addMarker( new MarkerOptions()
-                                    .position(new LatLng(lat, lon))
-                                    .title(type)
-                                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)))
-                                    .setTag(idReporte);
+                        //Añadir Marcadores
+                        switch (type) {
+                            case "Hurto":
+                                map.addMarker(new MarkerOptions()
+                                                .position(new LatLng(lat, lon))
+                                                .title(type)
+                                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)))
+                                        .setTag(idReporte);
+                                break;
+                            case "Actividad sospechosa":
+                                map.addMarker( new MarkerOptions()
+                                                .position(new LatLng(lat, lon))
+                                                .title(type)
+                                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)))
+                                        .setTag(idReporte);
 
-                            break;
-                        case "Asalto":
-                            map.addMarker(new MarkerOptions()
-                                    .position(new LatLng(lat, lon))
-                                    .title(type)
-                                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)))
-                                    .setTag(idReporte);
-                            break;
-                        case "Acoso":
-                            map.addMarker(new MarkerOptions()
-                                    .position(new LatLng(lat, lon))
-                                    .title(type)
-                                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)))
-                                    .setTag(idReporte);
-                            break;
-                        case "Secuestro":
-                            map.addMarker(new MarkerOptions()
-                                    .position(new LatLng(lat, lon))
-                                    .title(type)
-                                    .snippet("and snippet")
-                                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)))
-                                    .setTag(idReporte);
+                                break;
+                            case "Asalto":
+                                map.addMarker(new MarkerOptions()
+                                                .position(new LatLng(lat, lon))
+                                                .title(type)
+                                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)))
+                                        .setTag(idReporte);
+                                break;
+                            case "Acoso":
+                                map.addMarker(new MarkerOptions()
+                                                .position(new LatLng(lat, lon))
+                                                .title(type)
+                                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)))
+                                        .setTag(idReporte);
+                                break;
+                            case "Secuestro":
+                                map.addMarker(new MarkerOptions()
+                                                .position(new LatLng(lat, lon))
+                                                .title(type)
+                                                .snippet("and snippet")
+                                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)))
+                                        .setTag(idReporte);
 
-                            break;
-                        case "Tráfico de drogas":
-                            map.addMarker(new MarkerOptions()
-                                    .position(new LatLng(lat, lon))
-                                    .title(type)
-                                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)))
-                                    .setTag(idReporte);
-                            break;
-                        case "Tráfico de armas":
-                            map.addMarker(new MarkerOptions()
-                                    .position(new LatLng(lat, lon))
-                                    .title(type)
-                                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)))
-                                    .setTag(idReporte);
-                            break;
-                        case "disturbios":
-                            map.addMarker(new MarkerOptions()
-                                    .position(new LatLng(lat, lon))
-                                    .title(type)
-                                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)))
-                                    .setTag(idReporte);
-                            break;
-                    }
+                                break;
+                            case "Tráfico de drogas":
+                                map.addMarker(new MarkerOptions()
+                                                .position(new LatLng(lat, lon))
+                                                .title(type)
+                                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)))
+                                        .setTag(idReporte);
+                                break;
+                            case "Tráfico de armas":
+                                map.addMarker(new MarkerOptions()
+                                                .position(new LatLng(lat, lon))
+                                                .title(type)
+                                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)))
+                                        .setTag(idReporte);
+                                break;
+                            case "disturbios":
+                                map.addMarker(new MarkerOptions()
+                                                .position(new LatLng(lat, lon))
+                                                .title(type)
+                                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)))
+                                        .setTag(idReporte);
+                                break;
+                        }
                 }
 
                 if(!reports.isEmpty()){
-                    // Crea el heatmap
-                    HeatmapTileProvider provider = new HeatmapTileProvider.Builder()
-                            .data(reports)
-                            .radius(30)
-                            .build();
-                    // Añadir heatmap overlay al mapa
-                    TileOverlay overlay = map.addTileOverlay(new TileOverlayOptions().tileProvider(provider));
+                    // interpolar puntos
+                    for (int ixPoint = 0; ixPoint < reports.size() - 1; ixPoint++) {
+                        int nInterpolated = 15;
+                        for (int ixInterpolated = 0; ixInterpolated < nInterpolated; ixInterpolated++) {
+                            if ( SphericalUtil.computeDistanceBetween(new LatLng(reports.get(ixPoint).latitude, reports.get(ixPoint).longitude), new LatLng(reports.get(ixPoint + 1 ).latitude, reports.get(ixPoint + 1).longitude) ) <= 400) {
+                                LatLng interpolatedPoint = SphericalUtil.interpolate(reports.get(ixPoint),
+                                        reports.get(ixPoint + 1), (double) ixInterpolated / (double) nInterpolated);
+                                pathValuesInterpolated.add(interpolatedPoint.latitude + "," + interpolatedPoint.longitude);
+                            }
+
+                        }
+                    }
+
+                    // Snap to Roads
+                    try {
+                        run(pathValuesInterpolated, (reportsNew, readAllPaths) -> runOnUiThread(() -> {
+
+                            if (readAllPaths) {
+                                for (int i = 0; i < reports.size(); i++) {
+                                    reportsNew.add(new WeightedLatLng(new LatLng(reports.get(i).latitude, reports.get(i).longitude), 2));
+                                }
+
+                                // Create the gradient.
+                                int[] colors = {
+                                        Color.GREEN,    // green(0-50)
+                                        Color.YELLOW,    // yellow(51-100)
+                                        Color.rgb(255,165,0), //Orange(101-150)
+                                        Color.RED,              //red(151-200)
+                                        Color.rgb(153,50,204), //dark orchid(201-300)
+                                        Color.rgb(165,42,42) //brown(301-500)
+                                };
+
+                                float[] startPoints = {
+                                        0.1F, 0.2F, 0.3F, 0.4F, 0.6F, 1.0F
+                                };
+
+                                Gradient gradient = new Gradient(colors, startPoints);
+
+                                // Crea el heatmap
+                                HeatmapTileProvider provider = new HeatmapTileProvider.Builder()
+                                        .weightedData(reportsNew)
+                                        .radius(25)
+                                        .opacity(0.9)
+                                        .gradient(gradient)
+                                        .maxIntensity(25)
+                                        .build();
+                                // Añadir heatmap overlay al mapa
+                                TileOverlay overlay = map.addTileOverlay(new TileOverlayOptions().tileProvider(provider));
+                                map.setOnCameraMoveListener(() -> {
+                                    CameraPosition cameraPosition = map.getCameraPosition();
+                                    if(cameraPosition.zoom < 14) {
+                                        provider.setRadius(10);
+                                    } else if(cameraPosition.zoom < 16) {
+                                        provider.setRadius(25);
+                                    } else if(cameraPosition.zoom >= 16) {
+                                        provider.setRadius(50);
+                                    }
+                                    // Log.d(TAG, "onCameraMove: " + cameraPosition);
+                                });
+                            }
+                        }));
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
                 }
             }
 
@@ -361,6 +439,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
     }
+
     public void buscarCalle(View view) {
         // Se usa para cerrar el teclado.
         InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
@@ -408,6 +487,59 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         {
             Toast.makeText(this, "Escriba una dirección", Toast.LENGTH_SHORT).show();
 
+        }
+    }
+
+    //Interface callback 
+    interface RevealDetailsCallbacks {
+        void getDataFromResult(List<WeightedLatLng> reportsNew, boolean readAllPaths);
+    }
+    private final OkHttpClient client = new OkHttpClient();
+    public void run(List<String> pathValues, final RevealDetailsCallbacks revealDetailsCallbacks) throws Exception {
+        List<WeightedLatLng> reportsWeighted = new ArrayList<>();
+        final int[] readAllPaths = {0};
+
+        for(int i=0; i < pathValues.size(); i+=100){
+            List<String> subPath = pathValues.subList(i, Math.min(pathValues.size(),i+100) );
+            String url = "https://roads.googleapis.com/v1/nearestRoads?points=" + String.join("|", subPath) + "&key=" + MAPS_API_KEY;
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+
+            client.newCall(request).enqueue(new Callback() {
+                @Override public void onFailure(Call call, IOException e) {
+                    e.printStackTrace();
+                }
+
+                @Override public void onResponse(Call call, Response response) throws IOException {
+                    try (ResponseBody responseBody = response.body()) {
+                        if (!response.isSuccessful()) throw new IOException("MapActivity error: " + response);
+
+                        if (responseBody != null) {
+                            try {
+                                String jsonData = responseBody.string();
+                                JSONObject Jobject = new JSONObject(jsonData);
+                                JSONArray postsArray = Jobject.getJSONArray("snappedPoints");
+                                //Parse Json
+                                for (int i = 0; i < postsArray.length(); i++) {
+                                    JSONObject postObject = postsArray.getJSONObject(i);
+                                    double lat = postObject.getJSONObject("location").getDouble("latitude");
+                                    double lon = postObject.getJSONObject("location").getDouble("longitude");
+                                    reportsWeighted.add(new WeightedLatLng(new LatLng(lat, lon), 1));
+                                }
+                                if (revealDetailsCallbacks != null){
+                                    readAllPaths[0] += subPath.size();
+                                    revealDetailsCallbacks.getDataFromResult(reportsWeighted, readAllPaths[0] == pathValues.size());
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        } else {
+                            Log.d(TAG, "failed snapped points");
+                        }
+                    }
+                }
+            });
         }
     }
 }
